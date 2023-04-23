3d719add5747912756bd819b6c223d73489b8d4f
GabrielThurau <gjthurau92@yahoo.com>
2023-04-22 20:37:39 -0700

    Update script.js

--- odin-project/admin-dashboard/script.js
+++ odin-project/admin-dashboard/script.js
@@ -26,7 +26,12 @@ class Card {
                  <title>delete</title>
                  <path
                      d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
-             </svg></div>`;
+             </svg>
+             <svg class="plus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+             <title>plus</title>
+             <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+         </svg>
+             </div>`;
 
      createdCard.innerHTML = markup;
      document.querySelector('.cards').appendChild(createdCard);
@@ -57,7 +62,6 @@ class Store {
     }
 
     static clear() {
-
         localStorage.clear();
         console.log('local storage cleared');
     }
