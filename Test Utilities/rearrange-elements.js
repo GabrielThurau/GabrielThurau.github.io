fe89411e5cdefcce1aa3c52c9627dbeddd1258eb
GabrielThurau <gjthurau92@yahoo.com>
2023-05-28 10:11:07 -0700

    Update rearrange-elements.js

--- Test Utilities/rearrange-elements.js
+++ Test Utilities/rearrange-elements.js
@@ -1,8 +1,8 @@
 // for a random client site - https://humann.com/products/superbeets-soft-chews?variant=20791397711983
-// The goal of this code snippet is a clean way to first wait for elements to load on the page...
+// The goal of this code snippet is write a clean way to wait for elements to load on the page...
 //and then reorder them in a given way to test their optimal position on the page for increased conversions
 
-// 1. first we wait until the elements are ready.
+// 1. First we wait until the elements are ready.
 
 const sleepUntil = async (f, timeoutMs) => {
     return new Promise((resolve, reject) => {
@@ -10,7 +10,6 @@ const sleepUntil = async (f, timeoutMs) => {
       const wait = setInterval(function() {
         if (f()) {
           console.log("resolved after", new Date() - timeWas, "ms");
-          console.log('the element was found');
           clearInterval(wait);
           resolve();
         } else if (new Date() - timeWas > timeoutMs) { // Timeout
@@ -21,27 +20,21 @@ const sleepUntil = async (f, timeoutMs) => {
       }, 20);
     });
   }
+
+  //declare the elements in a variable for safekeeping within the try block. This is where you will do the thing.
   
   try {
-      await sleepUntil(() => document.querySelector("#shopify-section-template--15582584471691__product-guarantee"), 5000);
-      // ready
+      let guaranteeSection = document.querySelector("#shopify-section-template--15582584471691__product-guarantee");
+      let testimonialsSection = document.querySelector("#shopify-section-template--15582584471691__1644812433ee8ffcc4 > testimonials-section");
+      await sleepUntil(() => guaranteeSection, 2000);
+      await sleepUntil(() => testimonialsSection, 1000);
+      // 2. Then we 
+ 
   } catch {
-      // timeout
+    console.log('Error. Element took too long to load.')
+      // limit reached. Error will be thrown because it took too long to load the element
   }
 
-// 2. Then we define the check() callback that will fire once the condition has been met
-
-// const foo = async (evt, callback) => {
-//     // do something with evt
-//     // return response with callback
-//   }
 
 
-const check = function() {
-    let node1 = document.querySelector("#shopify-section-template--15582584471691__product-guarantee");
-    let node2 = document.querySelector("#shopify-section-template--15582584471691__1644812433ee8ffcc4");
-    if (node1 && node2) {
-        console.log('both elements were found');
-    }
-}
 
