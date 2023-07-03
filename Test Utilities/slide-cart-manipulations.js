f2247a335705f8a9f306ecac66cdbc5b3f4c4fad
GabrielThurau <gjthurau92@yahoo.com>
2023-07-02 21:07:06 -0700

    Update slide-cart-manipulations.js

--- Test Utilities/slide-cart-manipulations.js
+++ Test Utilities/slide-cart-manipulations.js
@@ -1,3 +1,8 @@
+//turn into classes? 
+
+// 
+
+
 
 const buttonID = setInterval(buttonCallback, 200);
 
@@ -24,9 +29,6 @@ function waitFor(selector) {
         function waitForElementToDisplay(selector, time) {
             if (document.querySelector(selector) != null) {
                 res(document.querySelector(selector));
-                console.log('found element');
-                console.log(selector);
-              
                 removeListeners(selector);
             }
             else {
@@ -50,22 +52,21 @@ if (window.location.href.indexOf("products") > -1) {
 }
 
 function removeListeners(selector) {
-  
   document.querySelector(selector).removeEventListener('click', redirectCart);
   document.querySelector(selector).removeEventListener('click', stopProp);
 }
 
 //gets discount total on load. And then pass it callback function to create HTML and append line item HTML
 
-// window.SLIDECART_LOADED = function(cart) {
-//     let cartItems = cart.items;
-//   let emptyArray = [];
-//    cartItems.forEach(item => {
-//     emptyArray.push(item.properties.Discount);
-//   });
-//   updateSavings();
-//   appendHTML();
-// }
+window.SLIDECART_LOADED = function(cart) {
+  let cartItems = cart.items;
+  let emptyArray = [];
+   cartItems.forEach(item => {
+    emptyArray.push(item.properties.Discount);
+  });
+  updateSavings(emptyArray);
+}
+
 
 
 window.SLIDECART_UPDATED = function(cart) {
@@ -74,25 +75,49 @@ window.SLIDECART_UPDATED = function(cart) {
    cartItems.forEach(item => {
     emptyArray.push(item.properties.Discount);
   });
-  console.log(cart.items[0].properties.Discount);
   updateSavings(emptyArray);
 }
 
-
 function appendHTML() {
+
+  rowMarkup = 
+  
+  `
+  <div class="footer-row cloned-acadia"><span class="flex">Discount</span><span class="shipping-cost"><span>random text here</span></span></div>
+  `
   console.log('html appended');
+  let slideFooter = document.querySelector("footer.footer");
+  let footerRow = slideFooter.querySelector('.footer-row');
+
+  let clonedRow = footerRow.cloneNode(true);
+  clonedRow.innerHTML = rowMarkup; 
+  document.querySelector("footer.footer > div:nth-child(1)").insertAdjacentHTML('afterend', clonedRow);
 }
 
 function updateSavings(fullArray) {
+  let clonedRow = document.querySelector('.cloned-acadia');
   const totalPrices = fullArray.map(x => x.split(' ')[1].replace(/[^0-9.-]+/g,""));
   let total = 0;
   let parsedInt;
   totalPrices.forEach(item => {
   parsedInt = parseInt(item);
   total += parsedInt;
-  updateHTML();
+  if (clonedRow) {
+    updateHTML(total);
+    return
+  }
+
+  else {
+    appendHTML(total);
+    updateHTML(total);
+  }
+
   });
 
   console.log(total);
 
+}
+
+function updateHTML(total) {
+  document.querySelector
 }
