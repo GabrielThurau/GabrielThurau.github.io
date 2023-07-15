970a34a559ed4aeb8ad14c3c3a5003246c77a4b9
GabrielThurau <gjthurau92@yahoo.com>
2023-07-14 22:09:18 -0700

    edits

--- Test Utilities/adding-slidecart.js
+++ Test Utilities/adding-slidecart.js
@@ -1,3 +1,3 @@
-// This is for testing slidecart on a shopify theme without actually having the functionality ovveride on cart.
+// This is for testing slidecart on a shopify theme without actually having the functionality override on cart.
 // This requires stopping immediate propagation on all buttons and then removing those events when accessed through Google Optimize container
 
